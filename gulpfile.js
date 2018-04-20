f4e92297ac616256de35cbfa585fcf34fd63def4
Katria Lesser <katrialynn18@gmail.com>
2018-04-20 11:11:01 -0600

    modified gulpfile to single combine css min

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
+// var minify = require('gulp-minify');
+var csso = require('gulp-csso');
 var autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
 
@@ -11,6 +13,7 @@ var paths = {
   styles: {
     input: 'src/styles/**/*.scss',
     output: 'dist/styles/'
+    // output: 'dist/styles/main.min.css'
   }
 };
 
@@ -20,9 +23,13 @@ gulp.task('styles', function () {
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({ browsers: ['last 2 versions'], remove: false, cascade: false }))
+    // Minify the file
+    .pipe(csso())
     .pipe(sourcemaps.write('.'))
     .pipe(cleanCSS())
-    .pipe(gulp.dest(paths.styles.output));
+    .pipe(concat('main.min.css'))
+    .pipe(gulp.dest('dist/styles/'));
+    // .pipe(gulp.dest(paths.styles.output));
 });
 
 // Build
